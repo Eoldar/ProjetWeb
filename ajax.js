a8536ee43975aa2d6cc2006767ca477f767c79a9
Eoldar <sutterguigui@hotmail.fr>
2017-02-02 13:34:23 +0100

    Etape 2 OK, Checker le trait lors du tour de la Terre

--- ajax.js
+++ ajax.js
@@ -11,12 +11,14 @@
 	L.control.scale().addTo(map);
 
 	var markers = new L.FeatureGroup();
- 	var i=0;
 	var old_lng=0;
-	var position_ISS = new Array(new Array());
+	var position_ISS = new Array();
+	var indice_trait = new Array();
+	indice_trait.push(0);
 	var latitude=0;
 	var longitude=0;
 	var zoom=0;
+	var i=0;
 
 	function send_request(){
 		var ajax = new XMLHttpRequest();
@@ -35,14 +37,17 @@
 				var coords=document.getElementById("coordinates");
 				coords.innerHTML="The ISS' coordinates are <br> Latitude : "+ latitude + " ---- Longitude : " + longitude;
 
-				if (position_ISS[i]!=0 && old_lng<=180 && old_lng>=170 && longitude>=-180 && longitude<=-170){i++;};
-				position_ISS[i].push(L.latLng(latitude, longitude));
+				if (position_ISS.length!=0 && old_lng<=180 && old_lng>=170 && longitude>=-180 && longitude<=-170){indice_trait.push(position_ISS.length);i=0;};
+				i++;
+				position_ISS.push(L.latLng(latitude, longitude));
 				old_lng=longitude;
 
-				for (var j=0; j<position_ISS.length;j++){
-					var ligne=L.polyline(position_ISS[i], {color:'red', weight: 5, opacity:1});
+				for (var j=0; j<indice_trait.length-1;j++){
+					var ligne=L.polyline(position_ISS.slice(indice_trait[j],indice_trait[j+1]-1), {color:'red', weight: 5, opacity:1});
 					markers.addLayer(ligne);
 				};
+				var ligne_current=L.polyline(position_ISS.slice(indice_trait[indice_trait.length-1],indice_trait[indice_trait.length-1]+i), {color:'red', weight: 5, opacity:1});
+				markers.addLayer(ligne_current);
 
 				map.addLayer(markers);
 			};});
@@ -60,7 +65,7 @@
 	};
 
 
-	setInterval(function(){send_request()},1000);
+	setInterval(function(){send_request()},5000);
 
 
 	var zoom_picture_ISS=document.getElementById("picture_submit");
@@ -79,18 +84,53 @@
 			var picture_ISS=document.getElementById("picture")
 			picture_ISS.innerHTML="<img src='https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/static/"+longitude+","+latitude+","+zoom+","+Math.random()*360+",50/600x400?access_token=pk.eyJ1IjoiZW9sZGFyIiwiYSI6ImNpeW4xOG1hMjAwNGozM3FsYnFheWJzOXYifQ.hZAFQxA9xQWMObZqfWdtog'/>"
 			var ajax2 = new XMLHttpRequest();
-			ajax2.open('GET', "http://api.geonames.org/findNearbyPlaceNameJSON?lat="+latitude+"&lng="+longitude+"&username=eoldar", true);
+			ajax2.open('GET', "http://api.geonames.org/extendedFindNearby?lat="+latitude+"&lng="+longitude+"&username=eoldar", true);
 			ajax2.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 			ajax2.addEventListener('readystatechange',  function(e) {
 				if(ajax2.readyState == 4 && ajax2.status == 200) {
-					response=JSON.parse(ajax2.responseText);
+					response=ajax2.responseXML;
+					var Json=xmlToJson(response);
 					var message=document.getElementById("message");
-					if (response.geonames.length==0){
-						message.innerHTML="Hello World";}
+					if (Json.geonames['#text'].length==2){
+						message.innerHTML="Hello "+Json.geonames.ocean.name['#text'];}
 					else {
-						message.innerHTML="Hello "+response.geonames[0].name+", "+response.geonames[0].countryName;}
+						message.innerHTML="Hello "+Json.geonames.geoname[4].toponymName['#text']+", "+Json.geonames.geoname[2].toponymName['#text']+", "+Json.geonames.geoname[1].toponymName['#text'];}
 				}
 			});
 			ajax2.send();
 		}
 	})
+
+
+
+	function xmlToJson(xml) {
+		var obj = {};
+		if (xml.nodeType == 1) {
+			if (xml.attributes.length > 0) {
+			obj["@attributes"] = {};
+				for (var j = 0; j < xml.attributes.length; j++) {
+					var attribute = xml.attributes.item(j);
+					obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+				}
+			}
+		} else if (xml.nodeType == 3) {
+			obj = xml.nodeValue;
+		}
+		if (xml.hasChildNodes()) {
+			for(var i = 0; i < xml.childNodes.length; i++) {
+				var item = xml.childNodes.item(i);
+				var nodeName = item.nodeName;
+				if (typeof(obj[nodeName]) == "undefined") {
+					obj[nodeName] = xmlToJson(item);
+				} else {
+					if (typeof(obj[nodeName].push) == "undefined") {
+						var old = obj[nodeName];
+						obj[nodeName] = [];
+						obj[nodeName].push(old);
+					}
+					obj[nodeName].push(xmlToJson(item));
+				}
+			}
+		}
+		return obj;
+	};
